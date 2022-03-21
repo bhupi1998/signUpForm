5b242a71483652c1a41eb003df2f8f5d9c8b3160
bhupi1998 <bhupi98.bs@gmail.com>
2022-03-20 23:25:43 -0700

    Good enough for now. This was a css practice project. No need to spend more time developing. Might come back in the future and add proper back end

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 let passWordMessage=document.getElementById("passwordNoMatch");
+let passwordInput=document.getElementById("password");
+let passwordConfirmInput=document.getElementById("passwordConfirm");
 
 passWordMessage.style.display="none";
